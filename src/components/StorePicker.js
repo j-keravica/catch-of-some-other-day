083185d395fd79e8a6df934306e842a3ff41a670
Jelena Keravica <jelena.keravica@gmail.com>
2018-03-11 11:24:55 +0100

    Add redirection to a specific store
    This commit introduces Context and how router can be accessed on a lower
    level component. StorePicker component now redirects to a store with the
    actual storeId. This is where the 12th video ends.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,7 +4,9 @@ import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
   goToStore(event) {
     event.preventDefault();
-    console.log(this.storeInput.value);
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`);
+    this.context.router.transitionTo(`/store/${storeId}`);
   }
 
   render() {
@@ -17,6 +19,10 @@ class StorePicker extends React.Component {
       </form>
     );
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;

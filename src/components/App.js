ab9f19bdad935e216dc6a7c59dca327ab843b99b
Jelena Keravica <jelena.keravica@gmail.com>
2018-03-12 14:13:36 +0100

    Implement Order component
    Add to order button now adds a specific fish to the order part of the
    application state. Order component shows what is included in the order.
    This is where the 17th video ends.

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,9 @@ import sampleFishes from '../sample-fishes';
 class App extends React.Component {
   constructor() {
     super();
-    this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addFish = this.addFish.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
     this.state = {
       fishes: {},
       order: {},
@@ -30,6 +31,12 @@ class App extends React.Component {
     this.setState({ fishes: fishes });
   }
 
+  addToOrder(key) {
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order: order });
+  }
+
   render() {
     return(
       <div className="catch-of-the-day">
@@ -39,11 +46,14 @@ class App extends React.Component {
             {
               Object
                 .keys(this.state.fishes)
-                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+                .map(key => <Fish key={key}
+                  id={key}
+                  details={this.state.fishes[key]}
+                  addToOrder={this.addToOrder}/>)
             }
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
-  render() {
+  constructor() {
+    super();
+    this.renderOrderItems = this.renderOrderItems.bind(this);
+  }
+
+  renderOrderItems(key) {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+
+    if(!fish || fish.status === 'unavailable') {
+      return(
+        <li key={key}>
+          Sorry, {fish ? fish.name : 'fish'} is no longer available.
+        </li>
+      );
+    }
+
     return(
-      <p>Order</p>
+      <li key={key}>
+        <span>{count}lbs {fish.name}</span>
+        <span className="price">{formatPrice(count * fish.price)}</span>
+      </li>
+    );
+  }
+
+  render() {
+    const { fishes, order } = this.props;
+    const orderIds = Object.keys(order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const count = order[key];
+      const fish = fishes[key];
+      if (fish && fish.status === 'available') {
+        return prevTotal + (count * fish.price);
+      }
+      return prevTotal;
+    }, 0);
+    return(
+      <div className="order-wrap">
+        <h2>Your Order</h2>
+        <ul className="order">
+          {orderIds.map(this.renderOrderItems)}
+          <li className="total">
+            <strong>Total:</strong>
+            {formatPrice(total)}
+          </li>
+        </ul>
+      </div>
     )
   }
 }

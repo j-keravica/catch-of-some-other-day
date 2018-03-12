1a619b72aa8ad8f9b2a4fc3f8cdb5026fc06c3b0
Jelena Keravica <jelena.keravica@gmail.com>
2018-03-12 14:41:48 +0100

    Add Firebase
    A Firebase connection is now added and the fifhes part of the state is
    being synced for each store. This is where the 18th video ends.

---
+++ src/base.js
@@ -0,0 +1,8 @@
+import Rebase from 're-base';
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBR40KLj0e7O8ZEIhxN0pGgGQy42ZZDMoo",
+  authDomain: "catch-of-some-other-day.firebaseapp.com",
+  databaseURL: "https://catch-of-some-other-day.firebaseio.com",
+});
+
+export default base;
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+import base from '../base';
 
 class App extends React.Component {
   constructor() {
@@ -15,6 +16,17 @@ class App extends React.Component {
       fishes: {},
       order: {},
     }
+  }
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`,
+      {
+        context: this,
+        state: 'fishes'
+      });
+  }
+
+  componentWillUnMount() {
+    base.removeBinding(this.ref);
   }
 
   loadSamples() {
